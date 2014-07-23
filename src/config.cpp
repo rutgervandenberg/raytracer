5aa151d31cb198465b85ffa6aba9ab2f5f93311e
Rutger van den Berg <rutgervandenberg@hotmail.com>
2014-07-23 17:21:05 +0200

    Refactor config.

--- src/config.cpp
+++ src/config.cpp
@@ -2,47 +2,39 @@
 #include <fstream>
 #include "config.hpp"
 #include <iostream> 
-class Config {
+
+Config::Config(std::string configname) {
 	std::unordered_map<std::string, int*> allowed_variables = {
-		{"MSAA", &MSAA},
-		{"WINDOW_RES_X", &WINDOW_RES_X},
-		{"WINDOW_RES_Y", &WINDOW_RES_Y},
-		{"RAYTRACE_RES_X", &RAYTRACE_RES_X},
-		{"RAYTRACE_RES_Y", &RAYTRACE_RES_Y},
-		{"VISIBILITY_TRESHOLD", &VISIBILITY_TRESHOLD}
+			{"MSAA", &MSAA},
+			{"WINDOW_RES_X", &WINDOW_RES_X},
+			{"WINDOW_RES_Y", &WINDOW_RES_Y},
+			{"RAYTRACE_RES_X", &RAYTRACE_RES_X},
+			{"RAYTRACE_RES_Y", &RAYTRACE_RES_Y},
+			{"VISIBILITY_TRESHOLD", &VISIBILITY_TRESHOLD}
 	};
 
-public: 
+	std::ifstream in(configname);
+	std::string optionname;
+	int* optionptr;
+	int value;
 
-	int MSAA = 0;
-	int WINDOW_RES_X = 0;
-	int WINDOW_RES_Y = 0;
-	int RAYTRACE_RES_X = 0;
-	int RAYTRACE_RES_Y = 0;
-	int VISIBILITY_TRESHOLD = 0;
+	while (in >> optionname) {
+		try {
+			optionptr = allowed_variables.at(optionname);
+		} catch (const std::out_of_range& oor) {
+			std::cerr << "Illegal option \"" << optionname << "\" in"
+					<< configname << ": " << oor.what() << '\n';
+			continue;
+		}
+		in >> value;
 
-
-	Config(string configname) {
-		std::ifstream in(configname);
-		std::string optionname;
-		int* optionptr;
-		int value; 
-
-		while (in >> optionname) {
-			try {
-				optionptr = allowed_variables.at(optionname);
-			} catch (const std::out_of_range& oor) {
-	    		std::cerr << "Illegal option \"" << optionname << "\" in" << configname << ": " << oor.what() << '\n';
-	    		continue;
-	  		}
-	  		in >> value;
-
-	  		if(!in.good()) {
-	  			std::cout << "Incorrect config file: " << configname << " at option: " << optionname << '\n';
-	  		} else {
-	  			*optionptr = value;
-	  		}
-		} 
-		in.close();
+		if (!in.good()) {
+			std::cout << "Incorrect config file: " << configname
+					<< " at option: " << optionname << '\n';
+		} else {
+			*optionptr = value;
+		}
 	}
+	in.close();
 }
+
--- src/config.hpp
+++ src/config.hpp
@@ -1,5 +1,11 @@
 #pragma once
-class Config{
-public: 
-	Config(string configname); 
+class Config {
+public:
+	Config(std::string configname);
+	int MSAA = 0;
+	int WINDOW_RES_X = 0;
+	int WINDOW_RES_Y = 0;
+	int RAYTRACE_RES_X = 0;
+	int RAYTRACE_RES_Y = 0;
+	int VISIBILITY_TRESHOLD = 0;
 };
